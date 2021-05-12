929dd7ab66187071c1d5ae9469276115538cb670
joycethomas-123 <78149448+joycethomas-123@users.noreply.github.com>
2021-05-12 21:54:57 +0530

    Create Day1a.c

---
+++ Day1a.c
@@ -0,0 +1,14 @@
+//Write a C Program to print "Hello World!" and "Welcome to Learn To Code!" in two separate lines
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello World! \n" );
+    printf("Welcome to Learn To Code!" );
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
