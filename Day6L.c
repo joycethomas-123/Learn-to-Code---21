cdf9650de50c2b2daf763e9fbc454321a06ae604
joycethomas-123 <78149448+joycethomas-123@users.noreply.github.com>
2021-05-17 14:48:45 +0530

    Create Day6L.c

---
+++ Day6L.c
@@ -0,0 +1,18 @@
+/*WACP to determine if a triangle is valid from the given sides*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+float a,b,c;
+    scanf("%f%f%f",&a,&b,&c);
+    
+    if ((a+b)>c)
+        printf("Yes");
+    else
+        printf("No");
+      
+    return 0;
+}
