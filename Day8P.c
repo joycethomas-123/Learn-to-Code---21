8adcc43fdf55af39bf2cadcf440ba6de2194201d
joycethomas-123 <78149448+joycethomas-123@users.noreply.github.com>
2021-05-23 23:11:01 +0530

    Create Day8P.c

---
+++ Day8P.c
@@ -0,0 +1,34 @@
+/*Write a C Program to compute the multiplication table of 1 - 5 using do-while.*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+   do
+   {
+       int i,n;
+       printf("\n1.Table of 1 \n2.Table of 2 \n3.Table of 3 \n4.Table of 4 \n5.Table of 5 \n6.Exit \n");
+       scanf("%d",&n);
+       if(n==6)
+       {
+           exit(0);
+       }
+       if(n>6)
+       {
+           printf("Enter a valid option!");
+       }
+       if(n<6)
+       {
+            for(i=1;i<=10;i++)
+            {
+              printf("%d ",n*i);
+            }
+       }
+   }while(1);
+
+       
+    return 0;
+}
